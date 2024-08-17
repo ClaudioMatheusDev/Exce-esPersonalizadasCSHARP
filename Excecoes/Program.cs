fe1bf733de5a9b3b7b8f520f96d31689affc472e
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-17 11:13:11 -0300

    ADD: Adicionando no program, entrada de dados, número do quarto, ddate de checkIN checkOut

--- Excecoes/Program.cs
+++ Excecoes/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Excecoes.Entities;
+
+Console.Write("Room number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Check-in date (dd/MM/yyyy): ");
+DateTime checkIn = DateTime.Parse(Console.ReadLine());
+Console.Write("Check-out date (dd/MM/yyyy): ");
+DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+if (checkOut <= checkIn)
+{
+    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+}
+else
+{
+    Reservation reservation = new Reservation(number, checkIn, checkOut);
+    Console.WriteLine("Reservation: " + reservation);
+}
