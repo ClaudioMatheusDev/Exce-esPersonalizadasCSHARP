b20d8e51cf7b33bd04027ac4aaea47968d51b4a2
Claudio Matheus <claudiomatheus055@gmail.com>
2024-08-17 11:06:14 -0300

    ADD: Construtores, Duration and retornando a reserva do usuario

---
+++ Excecoes/Entities/Reservation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excecoes.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+
+        public Reservation() { 
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }//CONSTRUTORES
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays; //RETORNANDO A DATA EM DIAS
+
+        }//PEGANDO O TEMPO QUE SOBRA DO CHECKIN MENOS O CHECKOUT
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out:  "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }//RETORNANDO O NÚMERO DO QUARTO, DATA DE CHECKIN, CHECKOUT E A DURAÇÃO DE TEMPO ENTRE ELES.
+    }
+}
